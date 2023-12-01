ee8bccde8aa913bdac2b0c83e67906bca0fc7061
Votre nom <votre-email@example.com>
2023-12-01 03:32:41 +0100

    Initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,88 @@
-ScrollReveal().reveal('.titre');
-
-
-document.getElementById('burgerBtn').addEventListener('click', () => {
-    document.getElementById('menuItems').classList.toggle('hidden');
+document.getElementById('burger-menu').addEventListener('click', function () {
+let mobileMenu = document.getElementById('mobile-menu');
+mobileMenu.classList.toggle('hidden');
+let menuIcon = document.querySelector('.menu-icon');
+menuIcon.innerHTML = (mobileMenu.classList.contains('hidden')) ?
+    '<svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>' :
+    '<svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>';
 });
-  
-// Fonctions pour le carrousel
-let currentIndex = 0;
-const testimonials = [
-  "<em>Message d'avis 1.</em>",
-  "<em>Message d'avis 2.</em>",
-  "<em>Message d'avis 3.</em>"
-];
 
-const showTestimonial = (index) => {
-  document.getElementById('testimonial').innerHTML = testimonials[index];
-};
+let currentSlide = 0;
+const testimonials = document.querySelectorAll('.testimonial');
 
-const nextSlide = () => {
-  currentIndex = (currentIndex + 1) % testimonials.length;
-  showTestimonial(currentIndex);
-};
+function showSlide(index) {
+    testimonials.forEach((testimonial, i) => {
+        if (i === index) {
+            testimonial.classList.add('active');
+        } else {
+            testimonial.classList.remove('active');
+        }
+    });
+}
 
-const prevSlide = () => {
-  currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
-  showTestimonial(currentIndex);
-};
+function changeSlide(direction) {
+    currentSlide = (currentSlide + direction + testimonials.length) % testimonials.length;
+    showSlide(currentSlide);
+}
 
-const viewAll = () => {
-  // Vous pouvez mettre en œuvre la logique pour afficher tous les témoignages ici
-  // par exemple, en utilisant une fenêtre modale.
-};
+// Auto-advance every 5000 milliseconds (5 seconds)
+setInterval(() => {
+    changeSlide(1);
+}, 5000);
 
-const addTestimonial = () => {
-  // Vous pouvez mettre en œuvre la logique pour ajouter un témoignage ici
-  // par exemple, en utilisant un formulaire.
-};
+let slideIndex = 1;
+
+showSlides01(slideIndex);
 
 
 
-function toggleMenu() {
-    var menu = document.getElementById('menuItems');
-    var burgerIcon = document.querySelector('.block.w-5');
 
-    if (!open) {
-        menu.classList.remove('hidden');
-        burgerIcon.classList.add('rotate-45', '-translate-y-1.5');
-    } else {
-        menu.classList.add('hidden');
-        burgerIcon.classList.remove('rotate-45', '-translate-y-1.5');
+
+function plusSlides01(n) {
+
+    showSlides01(slideIndex += n);
+
+}
+
+
+
+function currentSlide01(n) {
+
+    showSlides01(slideIndex = n);
+
+}
+
+
+
+function showSlides01(n) {
+
+    var i;
+
+    var slides = document.getElementsByClassName("mySlides01");
+
+    var dots = document.getElementsByClassName("dot01");
+
+    if (n > slides.length) {slideIndex = 1}    
+
+    if (n < 1) {slideIndex = slides.length}
+
+    for (i = 0; i < slides.length; i++) {
+
+        slides[i].style.display = "none";  
+
     }
 
-    open = !open;
+    for (i = 0; i < dots.length; i++) {
+
+        dots[i].className = dots[i].className.replace(" active", "");
+
+    }
+
+    slides[slideIndex-1].style.display = "block";  
+
+    dots[slideIndex-1].className += " active";
+
 }
+
+
+
